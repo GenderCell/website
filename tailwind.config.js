9d96ff7a349c8d9fb9fb9565bb4c3c84f0da0dde
AkshatIITk <iitkakshat@gmail.com>
2024-07-04 23:54:02 +0530

    initial commit with tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./src/*.{jsx,js}",
+  "./src/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
