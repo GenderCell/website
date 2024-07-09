46745603abf83442cb9457f2b41c73aadc685dac
BAJAJDHRUV09 <dhruvbajaj904@gmail.com>
2024-07-10 00:39:13 +0530

    event changes

--- src/components/imageslider/imageslider.jsx
+++ src/components/imageslider/imageslider.jsx
@@ -1,9 +1,8 @@
-// src/ImageSlider.jsx
 import { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import './imageslider.css';
 
-const ImageSlider = ({ images }) => {
+const ImageSlider = ({ images, textHeight }) => {
   const [counter, setCounter] = useState(0);
   const intervalId = useRef(null);
 
@@ -24,15 +23,19 @@ const ImageSlider = ({ images }) => {
   }, []);
 
   return (
-    <div className="event-gallery">
+    <div className="event-gallery" style={{ height: textHeight }}>
       <div className="event-gallery-images" style={{ transform: `translateX(-${counter * 100}%)` }}>
         {images.map((image, index) => (
-          <img src={image} className="slide" key={index} alt={`Slide ${index}`} />
+          <img src={image} className="slide" key={index} alt={`Slide ${index}`} style={{ height: '100%', width: 'auto' }} />
         ))}
       </div>
       <div className="nav">
-        <button onClick={goPrev} className="nav-button">&lt;</button>
-        <button onClick={goNext} className="nav-button">&gt;</button>
+        <button onClick={goPrev} className="nav-button">
+          &lt;
+        </button>
+        <button onClick={goNext} className="nav-button">
+          &gt;
+        </button>
       </div>
     </div>
   );
@@ -40,6 +43,7 @@ const ImageSlider = ({ images }) => {
 
 ImageSlider.propTypes = {
   images: PropTypes.arrayOf(PropTypes.string).isRequired,
+  textHeight: PropTypes.number.isRequired, // Prop for the height of text content
 };
 
 export default ImageSlider;
