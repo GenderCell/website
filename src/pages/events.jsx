41007ebb0c47730032a45c72d9e6822d8f9a8cf7
rpyaduvanshi950 <rpyaduvanshi950@gmail.com>
2026-01-26 20:28:23 +0530

    update events

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -18,7 +18,7 @@ import lapata1 from "../assets/images_events/lapata1.png";
 import lapata2 from "../assets/images_events/lapata2.png";
 // import case from 'src/assets/images_events/case.png';
 import barbie1 from '../assets/images_events/barbie1.png'
-import barbie2 from '../assets/images_events/barbie2.png' 
+import barbie2 from '../assets/images_events/barbie2.png'
 import meme from '../assets/images_events/meme.png';
 import movie_screening2 from '../assets/images_events/movie4.jpeg';
 import wall_of_voices from '../assets/images_events/wall_of_voices.png';
@@ -53,7 +53,7 @@ const EventsPage = () => {
       id: 2,
       img: hall2,
     },
-    
+
     {
       id: 3,
       img: lapata1,
@@ -83,18 +83,18 @@ const EventsPage = () => {
   const [galleryData] = useState(galleryDB);
 
   const events = [
-     {
+    {
       subtitle: "Poetry Competition – Goonj: Echoes Unheard",
       description: `Date:  18th to 26th October, 2025 <br/>
 Venue: Senate Hall <br/> The Gender Cell organized Goonj: Echoes Unheard, a poetry competition that provided a platform for participants to express their thoughts on gender, identity, and social justice through verse. The event celebrated the power of words in amplifying unheard voices and fostering empathy, awareness, and dialogue on issues of equality and inclusion.`
- , image:
+      , image:
         goonj,
     },
-      {
+    {
       subtitle: "Sensitization Session at Opportunity School",
       description: `Date: 14th October, 2025 <br/>
 Venue: Opportunity School <br/>The Gender Cell conducted an interactive sensitization session for the students of Opportunity School, focusing on themes of respect, empathy, and equality. Through engaging discussions and activities, the session aimed to promote gender sensitivity and build awareness from a young age.`
-     , image:
+      , image:
         opportunity,
     },
 
@@ -113,7 +113,7 @@ Platform: Gender Cell Website and Email <br/>The Gender Cell, IIT Kanpur, is sta
 TOWARDS A SAFER SPACE
 ‘A monthly blog on gender sensitization and respectful conduct.’
 Each month, we’ll share short, thought-provoking pieces that highlight key issues around gender, consent, and creating an inclusive environment on campus. The goal is simple but vital: to build a culture where everyone feels safe, respected, and heard.`
-     , image:
+      , image:
         blogs,
     },
 
@@ -132,7 +132,7 @@ The event was a delightful Movie -evening that reminded everyone of the power of
 Venue: OAT <br/>The Gender Cell, IIT Kanpur successfully organized the 7th Edition of the 5KM Run & Walk on 22nd March 2025, witnessing an incredible turnout of 900+ participants. The event began at 5:30 AM from the Open Air Theatre (OAT) and featured a fun-filled 10-minute Zumba session to kick off the morning. The enthusiastic participation and vibrant energy made it a memorable celebration of health, unity, and empowerment.`
       , image:
         runwalk2,
-    },{
+    }, {
       subtitle: "Meme Competition on the theme Breaking Gender Stereotypes and Raising Awareness Against Sexual Harassment",
       description: ` <br/>
 Venue : Lecture Hall 17<br/>The Gender Cell, IIT Kanpur, organized a magical evening on 29th January 2025 with the screening of Barbie at Lecture Hall 17, drawing viewers into a world that challenges patriarchal norms, celebrates individuality, and empowers self-expression. Starting at 6:10 PM, the film sparked laughter, reflection, and meaningful conversations about identity, self-discovery, and breaking societal expectations. The event was a vibrant celebration of being unapologetically unique and empowered making it an inspiring night to remember.
@@ -143,15 +143,15 @@ Venue : Lecture Hall 17<br/>The Gender Cell, IIT Kanpur, organized a magical eve
       subtitle: "Women’s Day Talk by Ms. Seema Kushwaha",
       description: `Date: 8th March 2025 (Saturday) <br/>
 Venue : Lecture Hall 8<br/>TOn the occasion of International Women’s Day, the Gender Cell, IIT Kanpur organized a powerful and insightful talk by Ms. Seema Kushwaha, Supreme Court Advocate and legal counsel in the 2012 Nirbhaya case. Known for her relentless pursuit of justice, Ms. Kushwaha shared her experiences and perspectives that continue to inspire and empower generations`
-      , image:         womensDay1,
-    },{
+      , image: womensDay1,
+    }, {
       subtitle: "Movie Screening: Barbie",
       description: `Date: 29th January 2025 <br/>
 Venue : Lecture Hall 17<br/>The Gender Cell, IIT Kanpur, organized a magical evening on 29th January 2025 with the screening of Barbie at Lecture Hall 17, drawing viewers into a world that challenges patriarchal norms, celebrates individuality, and empowers self-expression. Starting at 6:10 PM, the film sparked laughter, reflection, and meaningful conversations about identity, self-discovery, and breaking societal expectations. The event was a vibrant celebration of being unapologetically unique and empowered making it an inspiring night to remember.
 `
-      , image: 
+      , image:
         barbie2,
-    },{
+    }, {
       subtitle: "ICC Case Correspondence Document",
       description: `Date: 4th Dec, 2024 <br/>
 Mode: Online <br/>The Gender Cell, IIT Kanpur, successfully released the ICC Case Correspondence Document, marking a significant milestone in promoting awareness, accountability, and institutional transparency into the enquiry process of complaints related to Sexual Harassment. 
@@ -163,29 +163,29 @@ The document provides a comprehensive overview of the nature of sexual offenses 
       subtitle: "Movie Screening: Laapata Ladies",
       description: `Date: October 15, 2024 <br/>
 Venue: L9 <br/>The Gender Cell, IIT Kanpur conducted a special screening of Laapata Ladies which witnessed a houseful audience till the very end. As India’s Oscar entry, the film delivered a witty yet profound narrative that challenged the gender norms in rural India. Through humor and heartfelt storytelling, it sparked deep reflections on societal stereotypes that have historically restricted women’s empowerment. The audience remained engaged throughout, and the film’s impact forced people into thought-provoking discussions, reinforcing the need for continuous dialogue on gender equality.`
-      , image: 
+      , image:
         lapata1,
-        
-      
+
+
     },
-    
+
     {
       subtitle: "Bridging Gaps: Hall-Level Sessions for Freshers'24",
       description: `Date: 30th Sep - 1st Oct <br/>
 Venue: Hall 13, 4, 14 and GH1 <br/>
 A Hall-level Gender Sensitization session was conducted by the Gender Cell, IIT Kanpur, for the first-year UG and PG students of Halls 13, 4, 14, and GH1. The session aimed to increase awareness about sexual harassment, gender biases, and the available methods for filing complaints. Participants were also informed about the wide range of support services that the Gender Cell offers to the campus community along with some real life success stories. Attendees actively engaged by asking important questions, which were addressed by the student members of the Gender Cell, fostering an open and informative discussion. This initiative was a key step towards creating a safer and more inclusive environment at IITK.`,
-      image: 
+      image:
         hall1,
-    },{
+    }, {
       subtitle: "Open Mic: Speaking Out, Standing Strong",
       description: `Date: 27 August 2024 <br> Time: 6:30 to 8:30 <br> Venue: L18-L19 Foyer <br> The Open Mic Night, conducted in association with the Institute Counselling Service, IITK was an impactful event where students openly shared their personal stories of harassment and mental stress, along with how they overcame these challenges. The evening was marked by a sense of unity as people came together to freely express their experiences, ensuring that every voice was heard and valued. The event underscored the importance of creating spaces for open dialogue and mutual support within the campus community.`,
-      image: 
+      image:
         open1,
     },
     {
       subtitle: "Degendering STEM: Why So Slow?",
       description: `Date: 8 March 2024 <br> Time: 5:00 PM<br>Speaker: Dr Prajval Shastri, Emeritus Scientist of Astronomy and Astrophysics, Raman Research Institute <br>Venue: L9<br>The talk focused on gender discrimination and the lack of representation of different genders in the area of STEM. The insightful talk was followed with a  Q&A session focusing on gender-sensitive policies and practices.<br>`,
-      image: 
+      image:
         womensDay1,
 
     },
@@ -196,14 +196,14 @@ A Hall-level Gender Sensitization session was conducted by the Gender Cell, IIT 
         Theme: "Don't be a Bystander; Stand Against Sexual Harassment"
 <br> Number of participants:  150+
 <br>`,
-      image: 
+      image:
         runwalk2,
-   
+
     },
     {
       subtitle: "Movie Screening followed by a discussion with the actor",
       description: "  Movie: Turup, directed by Ektara Collective <br> Interaction with Nidhi Qazi, the actor who played a leading role of the character Neelima in the movie. <br> Date: 10 March 2024<br>Time: 6:00 AM<br> <br>Venue: L2<br> The screening was followed by a thought-provoking and critical discussion on the movie by the audience. <br> ",
-      image: 
+      image:
         movie_screening2,
     },
   ];
@@ -221,18 +221,18 @@ A Hall-level Gender Sensitization session was conducted by the Gender Cell, IIT 
       <section className="section-event-gallery" id="section-event-gallery">
         <h2 className="heading1">Recent Events</h2>
         <div className="gallery">
-  <div className="main-gallery">
-    {events.map((event, index) => (
-      <EventsCard
-        key={index}
-        title={event.title}       // If not used inside EventsCard, can remove this prop
-        subtitle={event.subtitle}
-        description={event.description}
-        image={event.image}       // Single image prop here instead of sliderImages
-      />
-    ))}
-  </div>
-</div>
+          <div className="main-gallery">
+            {events.map((event, index) => (
+              <EventsCard
+                key={index}
+                title={event.title}       // If not used inside EventsCard, can remove this prop
+                subtitle={event.subtitle}
+                description={event.description}
+                image={event.image}       // Single image prop here instead of sliderImages
+              />
+            ))}
+          </div>
+        </div>
 
         <div className="app-gallery" id="gallery">
           <div className="app-gallery-content">
@@ -269,7 +269,7 @@ A Hall-level Gender Sensitization session was conducted by the Gender Cell, IIT 
           </div>
         </div>
       </section>
-      <LinksSection/>
+      <LinksSection />
     </div>
   );
 };
